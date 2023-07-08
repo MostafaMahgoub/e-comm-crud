563fcbe398c3c5f7725d0506fcbbf962c853aeed
MostafaR <mostafamahgoub@outlook.com>
2023-07-08 20:47:48 +0300

    Remove unused files and begin the project

---
+++ src/CRUD/crud.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function crud() {
+  return (
+    <div className="crud">
+      <header className="crud-header">
+        <p>
+          CRUD
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default crud;
---
+++ src/E-commerce/e-comm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ecomm() {
+  return (
+    <div className="ecomm">
+      <header className="ecomm-header">
+        <p>
+        ecomm
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default ecomm;
